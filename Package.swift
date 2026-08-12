ec79308e7e70d3854d5e9bea93868782ecf612c1
frett <47818+frett@users.noreply.github.com>
2026-08-12 17:50:07 +0000

    Update package

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   targets: [
       .binaryTarget(
         name: "GodToolsShared",
-        url: "https://cruglobal.jfrog.io/artifactory/swift-snapshots-local/CruGlobal/GodToolsShared/GodToolsShared-1.4.0-SNAPSHOT.31442844879.xcframework.zip",
-        checksum: "1b6f24d91ee1ee21406b6e6f753527c57ad3dee0e7ae1d9cac8c03be9370d2e9"
+        url: "https://cruglobal.jfrog.io/artifactory/swift-snapshots-local/CruGlobal/GodToolsShared/GodToolsShared-1.4.0-SNAPSHOT.31622730373.xcframework.zip",
+        checksum: "503f4e3de87bde88d8237c52563bf01904900f25448a50e8b5e010240bc3b5ae"
       )
   ]
 )
